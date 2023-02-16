a56c6d89ee2ca0a660ad50ccfa8db1b141d203a8
Seung taek Oh <elqkddl@naver.com>
2023-02-17 00:43:23 +0900

    onclick 사용법 예시

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+/*eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let[글제목,b] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
+  let[따봉] = useState(0)
+
+  function 함수(){
+    console.log(1);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color:'red', fontSize:'16px'}}>난 블로그임</h4>
+      </div >
+      <div className="list">
+        <h4>{글제목[0]} <span onClick={함수}>좋아요👍</span>{따봉}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
