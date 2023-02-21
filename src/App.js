626388bd9d41954854f4d6b7fe1ece7a3a7c87e7
Seung taek Oh <elqkddl@naver.com>
2023-02-22 01:34:26 +0900

    learn React에서 레이아웃 만들 때 쓰는 JSX 문법 3개

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 /*eslint-disable*/
 
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; // 경로 import 해야함
 import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
-  let[글제목,b] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
-  let[따봉] = useState(0)
+  let[글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
+  let[따봉,따봉변경] = useState(0)
 
   function 함수(){
     console.log(1);
   }
-
-
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:'red', fontSize:'16px'}}>난 블로그임</h4>
+        <h4>난 블로그임</h4>
       </div >
-      <div className="list">
-        <h4>{글제목[0]} <span onClick={함수}>좋아요👍</span>{따봉}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
