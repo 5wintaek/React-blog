f007fade93acdbcd4feca4382fc35da412c62e92
Seung taek Oh <elqkddl@naver.com>
2023-04-05 00:09:13 +0900

    [learn] useState 추가

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 export function Category() {
   const post = '글제목';
+  const [a, b] = useState('남자코트추천');
   return (
     <List>
-      <h4>{post}</h4>
+      <h4>{a}</h4>
       <p>2월17일 발행</p>
     </List>
   );
